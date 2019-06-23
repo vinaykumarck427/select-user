b4f1a7e6340e889e78de6d92ec45747f3da0afcd
vinay kumar c k <vinaykumarck427@gmail.com>
2019-06-23 23:19:36 +0530

    add css to center

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import {
+  Card, CardText, 
+  CardBody,
+  CardTitle
+} from 'reactstrap';
+import _ from 'lodash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      users:[],
+      id:'',
+      user:{},
+      selectName:''
+    }
+  }
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      console.log(response.data)
+      this.setState({users:response.data})
+    })
+  }
+  handleSelect = (e) => {
+    const name = e.target.value
+      axios.get(`https://jsonplaceholder.typicode.com/users?name=${name}`)
+        .then(response => {
+          console.log(response.data)
+          this.setState({ user: response.data, selectName:response.data.name})
+        })
+   
+  }
+  render(){
+    return(
+      <div>
+        <form>
+          <div className="form-group">
+            <label htmlFor="choose"><h2>Choose user</h2></label>
+            <select className="form-control" to="choose" onChange={this.handleSelect} value={this.state.selectName}>
+              <option value="">select</option>
+              {this.state.users.map(user => <option key={`${user.id}`} value={`${user.name}`}>{user.name}</option>)}
+            </select>
+          </div>
+        </form>
+        
+        <br />
+
+        {!_.isEmpty(this.state.user) && (
+          <Card>
+            <CardBody>
+              <CardTitle><b>{this.state.user[0].name}</b></CardTitle>
+              <CardText>
+                <p><b>Id:</b>{this.state.user[0].id}</p><br />
+                <p><b>Username:</b>{this.state.user[0].username}</p><br />
+                <p><b>Email:</b>{this.state.user[0].email}</p>
+              </CardText>
+            </CardBody>
+          </Card>
+        )}
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
